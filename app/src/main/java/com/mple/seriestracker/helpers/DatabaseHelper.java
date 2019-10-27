aeae4e606dd0c161bda2dd6a5b08682c0e558fa9
Julien Powell <julien.f.powell@gmail.com>
2019-10-27 13:25:06 +1300

    minor refactoring

--- app/src/main/java/com/mple/seriestracker/helpers/DatabaseHelper.java
+++ app/src/main/java/com/mple/seriestracker/helpers/DatabaseHelper.java
@@ -5,11 +5,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "SeriesTracker.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "SeriesTracker.db";
 
-    String SQL_Create_Entries;
-    String SQL_Delete_Entries;
+    private String SQL_Create_Entries;
+    private String SQL_Delete_Entries;
 
     public DatabaseHelper(Context context,String sqlCreate,String sqlDelete) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
